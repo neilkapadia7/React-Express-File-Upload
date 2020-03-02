34630638660454442a51f44a650e56b8e09e909d
neilkapadia7 <neilkapadia7@gmail.com>
2020-03-02 23:40:59 +0530

    Updated Front-End

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import './App.css';
 import FileUpload from './components/FileUpload';
+import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <h4><i className='fab fa-react' />React File Uploader</h4>
-      <FileUpload />
-    </div>
-  );
-}
+const App = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      <i className='fab fa-react' /> React File Upload
+    </h4>
+
+    <FileUpload />
+  </div>
+);
 
 export default App;
--- server.js
+++ server.js
@@ -7,20 +7,20 @@ app.use(fileUpload());
 
 // Upload Endpoint
 app.post('/upload', (req, res) => {
-    if(req.files === null) {
-        return res.status(400).json({ msg: 'No File Uploaded'});
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file uploaded' });
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
     }
 
-    const file= req.files.file;
-
-    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
-        if(err) {
-            console.error(err);
-            return res.status(500).send(err);
-        }
-    });
-
     res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
 });
 
-app.listen(5000, () => console.log('Server Started on Port Number 5000'));
+app.listen(5000, () => console.log('Server Started...'));
